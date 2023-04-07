c39ea5c3ecba613553d2422b7b4b654b2112bb13
xorik <xorik@users.noreply.github.com>
2023-04-07 17:17:11 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <!--- Tailwind classes -->
+  <h1 class="text-2xl font-bold text-green-500">Home page</h1>
+
+  <!-- Auto import for components -->
+  <HelloWorld msg="test"/>
+
+  <!-- Icons -->
+  <i-carbon-bee/>
+
+  <!-- Daisy UI -->
+  <button class="btn btn-primary"  >Button</button>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+}
+
