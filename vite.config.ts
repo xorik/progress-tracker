4b826d735ea80c02136e4e27ef8468e401d781bd
xorik <xorik@users.noreply.github.com>
2023-04-28 15:50:04 +0200

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,12 +22,40 @@ export default defineConfig({
     Icons({
       autoInstall: true,
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Progress tracker',
+        short_name: 'Tracker',
+        theme_color: '#1FB2A5',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          icons: ['src/assets/icons.json'],
+        manualChunks: (id) => {
+          if (id.includes('src/assets/icons.json')) {
+            return 'icons'
+          }
+
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
         },
       },
     }
